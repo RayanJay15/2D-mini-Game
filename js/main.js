c6794309a734d9353e023dbfe36d0fe56c34a010
rajitha jayarathne <rajithajayarathne94@gmail.com>
2023-06-03 21:09:52 +0530

    Run,jump,attack & dead animations have done

--- js/main.js
+++ js/main.js
@@ -48,8 +48,6 @@ for(let i = 0; i < 10; i++){
     image.src  = `img/Run__00${i}.png`;
 }
 
-const img=new Image();
-img.src='img/Jump__001.png';
 
 const boxElm = document.createElement('div');
 boxElm.classList.add('box');
@@ -57,14 +55,153 @@ document.getElementById('background').append(boxElm);
 
 document.body.addEventListener('click', ()=> document.body.requestFullscreen());
 
+let jump = false;
+let run = false;
+let attack=false;
+let jumpAttack=false;
+let dead=false;
+
+let dx = 0;
+
+document.body.addEventListener('keydown', (eventData)=> {
+    if (eventData.code === 'Space'){
+        boxElm.style.width='150px';
+        jump = true;
+    }else if (eventData.code === 'ArrowRight'){
+        boxElm.style.transform = 'rotateY(0deg)';
+        boxElm.style.width='150px';
+        run = true;
+        dx = 2;
+    }else if (eventData.code === 'ArrowLeft'){
+        boxElm.style.transform = 'rotateY(180deg)';
+        boxElm.style.width='150px';
+        run = true;
+        dx = -2;
+    }else if (eventData.code==='KeyE'){
+
+        boxElm.style.width='250px';
+        attack=true;
+
+    }else if(eventData.code=='KeyD'){
+        dead=true;
+    }
+   
+
+    console.log(eventData);
+    
+});
 
 
 
+document.body.addEventListener('keyup', (eventData) => {
+s
+    if (eventData.code === 'ArrowRight'){
+        boxElm.style.width='100px';
+        run = false;
+        dx = 0;
+    }else if (eventData.code === 'ArrowLeft'){
+        boxElm.style.width='100px';
+        run = false;
+        dx = 0;
+    }else if(eventData.code ==='KeyE'){
+        boxElm.style.width='100px';
+        attack=false;
+    }else if(eventData.code==='KeyT'){
+        thrw=true;
+    }else if(eventData.code==='Space'){
+        
+        if(boxElm.style.bottom>'70px'){
+            boxElm.style.width='150px';
+        }else if(boxElm.style.bottom==='70px'){
+            boxElm.style.width='100px';
+        }
+        
+        
+    }
+
+});
+
+let angle = 0;
+function doJump(){
+    let y  = Math.cos(angle * (Math.PI / 180));
+    y *= 3;
+    boxElm.style.top = (boxElm.offsetTop - y) + "px";
+    angle++;
+    if (angle >  180){
+        jump = false;
+        angle = 0;  
+    }
+}
+
+function doRun(){
+    let x = boxElm.offsetLeft + dx;
+    if ((x + boxElm.offsetWidth)> innerWidth) {
+        x = innerWidth - boxElm.offsetWidth;
+    }else if (x <= -250) x = -250;
+    
+    boxElm.style.left = `${x}px`;
+    
+}
 
 
 
-const background=document.createElement('div');
-background.style.backgroundColor='blue';
-background.style.width='100px';
-background.style.height='100px';
-document.body.append(background);
+let i = 0;
+function drawIdle(){
+    boxElm.style.backgroundImage = `url('img/Idle__00${i++}.png')`;
+    boxElm.style.boxSizing='content';
+    if(i === 9) i = 0;
+}
+
+let k = 0;
+function drawJump(){
+    boxElm.style.backgroundImage = `url('img/Jump__00${k++}.png')`;
+    if(k === 9) k = 0;
+}
+
+let j = 0;
+function drawRun(){
+    boxElm.style.backgroundImage = `url('img/Run__00${j++}.png')`;
+    if(j === 9) j = 0;
+}
+let a=0;
+function drawAttack(){
+    boxElm.style.backgroundImage =`url('img/Attack__00${a++}.png')`;
+    if(a==9) a=0;
+}
+let d=0;
+function drawDead(){
+    boxElm.style.backgroundImage =`url('img/Dead__00${d++}.png')`;
+    if(d==9) d=0;
+}
+
+
+setInterval(()=> {
+    if (jump){
+        doJump();
+    }
+    if (run){
+        doRun();
+    }
+    
+}, 5);
+
+setInterval(()=> {
+    if (!jump && !run && !attack && !dead) {
+        drawIdle();
+    }else if (jump){
+        drawJump();
+    }else if (!jump && !attack && run){
+        drawRun();
+    }else if (!run && attack){
+        drawAttack();
+    }else if (!run && jump && !attack && !dead){
+        drawJump();
+        
+    }
+
+} , (1000/20));
+
+
+setInterval(()=>{
+
+})
