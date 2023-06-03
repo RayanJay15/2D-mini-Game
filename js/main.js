55938be7b10048dd019963df35b5935e2b545641
rajitha jayarathne <rajithajayarathne94@gmail.com>
2023-06-03 17:36:20 +0530

    Initialize the project

---
+++ js/main.js
@@ -0,0 +1,57 @@
+/* Load ninja charcter splites*/
+for(let i = 1; i <= 12; i++){
+    const image = new Image();
+    image.src  = `img/Attack__00(${i}).png`;
+}
+for(let i = 1; i <= 10; i++){
+    const image = new Image();
+    image.src  = `img/Climb__00(${i}).png`;
+}
+for(let i = 1; i <= 10; i++){
+    const image = new Image();
+    image.src  = `img/Dead__00(${i}).png`;
+}
+for(let i = 1; i <= 10; i++){
+    const image = new Image();
+    image.src  = `img/Glide__00(${i}).png`;
+}
+for(let i = 1; i <= 10; i++){
+    const image = new Image();
+    image.src  = `img/idle__00(${i}).png`;
+}
+for(let i=1;i<=10;i++){
+    const image=new Image();
+    image.src =`img/Jump__00(${i}.png)`;
+}
+for(let i = 1; i <= 10; i++){
+    const image = new Image();
+    image.src  = `img/Jump_Attack__00(${i}).png`;
+}
+for(let i = 1; i <= 10; i++){
+    const image = new Image();
+    image.src  = `img/Jump_Throw__00(${i}).png`;
+}
+for(let i = 1; i <= 10; i++){
+    const image = new Image();
+    image.src  = `img/Run__00(${i}).png`;
+}
+for(let i = 1; i <= 10; i++){
+    const image = new Image();
+    image.src  = `img/Slide__00(${i}).png`;
+}
+for(let i = 1; i <= 10; i++){
+    const image = new Image();
+    image.src  = `img/Throw__00(${i}).png`;
+}
+
+
+
+
+
+
+
+const background=document.createElement('div');
+background.style.backgroundColor='yellow';
+background.style.width='100px';
+background.style.height='100px';
+document.body.append(background)
