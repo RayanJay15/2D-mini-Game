e6bf4326171593c56ec4ec7a9eb62f778c0ddae6
rajitha jayarathne <rajithajayarathne94@gmail.com>
2023-06-03 18:13:26 +0530

    Initialize main character

--- js/main.js
+++ js/main.js
@@ -1,48 +1,61 @@
 /* Load ninja charcter splites*/
-for(let i = 1; i <= 12; i++){
+for(let i = 0; i <= 9; i++){
     const image = new Image();
-    image.src  = `img/Attack__00(${i}).png`;
+    image.src  = `img/Attack__00${i}.png`;
 }
-for(let i = 1; i <= 10; i++){
+for(let i = 0; i < 10; i++){
     const image = new Image();
-    image.src  = `img/Climb__00(${i}).png`;
+    image.src  = `img/Climb_00${i}.png`;
 }
-for(let i = 1; i <= 10; i++){
+for(let i = 0; i < 10; i++){
     const image = new Image();
-    image.src  = `img/Dead__00(${i}).png`;
+    image.src  = `img/Dead__00${i}.png`;
 }
-for(let i = 1; i <= 10; i++){
+for(let i = 0; i < 10; i++){
     const image = new Image();
-    image.src  = `img/Glide__00(${i}).png`;
+    image.src  = `img/Glide_00${i}.png`;
 }
-for(let i = 1; i <= 10; i++){
+for(let i = 0; i < 10; i++){
     const image = new Image();
-    image.src  = `img/idle__00(${i}).png`;
+    image.src  = `img/Idle__00${i}.png`;
 }
-for(let i=1;i<=10;i++){
+for(let i=0;i<10;i++){
     const image=new Image();
-    image.src =`img/Jump__00(${i}.png)`;
+    image.src =`img/Jump__00${i}.png`;
 }
-for(let i = 1; i <= 10; i++){
+for(let i = 0; i < 10; i++){
     const image = new Image();
-    image.src  = `img/Jump_Attack__00(${i}).png`;
+    image.src  = `img/Jump_Attack__00${i}.png`;
 }
-for(let i = 1; i <= 10; i++){
+for(let i = 0; i < 10; i++){
     const image = new Image();
-    image.src  = `img/Jump_Throw__00(${i}).png`;
+    image.src  = `img/Jump_Throw__00${i}.png`;
 }
-for(let i = 1; i <= 10; i++){
+for(let i = 0; i < 10; i++){
     const image = new Image();
-    image.src  = `img/Run__00(${i}).png`;
+    image.src  = `img/Run__00${i}.png`;
 }
-for(let i = 1; i <= 10; i++){
+for(let i = 0; i < 10; i++){
     const image = new Image();
-    image.src  = `img/Slide__00(${i}).png`;
+    image.src  = `img/Slide__00${i}.png`;
 }
-for(let i = 1; i <= 10; i++){
+for(let i = 0; i < 10; i++){
     const image = new Image();
-    image.src  = `img/Throw__00(${i}).png`;
+    image.src  = `img/Throw__00${i}.png`;
 }
+for(let i = 0; i < 10; i++){
+    const image = new Image();
+    image.src  = `img/Run__00${i}.png`;
+}
+
+const img=new Image();
+img.src='img/Jump__001.png';
+
+const boxElm = document.createElement('div');
+boxElm.classList.add('box');
+document.getElementById('background').append(boxElm);
+
+document.body.addEventListener('click', ()=> document.body.requestFullscreen());
 
 
 
@@ -51,7 +64,7 @@ for(let i = 1; i <= 10; i++){
 
 
 const background=document.createElement('div');
-background.style.backgroundColor='yellow';
+background.style.backgroundColor='blue';
 background.style.width='100px';
 background.style.height='100px';
-document.body.append(background)
+document.body.append(background);
